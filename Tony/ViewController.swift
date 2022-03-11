88a6fb633580dafeb7ce5831bd4c008c7da39b50
yuoku <appledev.yuoku@gmail.com>
2022-03-12 01:07:43 +0900

    Create a new project.

---
+++ Tony/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tony
+//
+//  Created by Yuki Okudera on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
