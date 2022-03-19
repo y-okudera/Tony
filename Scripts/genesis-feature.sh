5c2f48a880780c88e641933fe10475f17ca29b53
yuoku <appledev.yuoku@gmail.com>
2022-03-19 13:40:17 +0900

    Update genesis script.

--- Scripts/genesis-feature.sh
+++ Scripts/genesis-feature.sh
@@ -8,6 +8,26 @@ echo $current_year
 developer_name="Yuki Okudera"
 developer_company="Yuki Okudera"
 
-swift run -c release --package-path ./Tools/GenesisTool genesis generate Genesis/Feature/template.yml\
-  -d Genesis/Feature/output/\
-  -o "date: $today, year: $current_year, developerName: $developer_name, developerCompany: $developer_company"
+read -p "new feature scene name to generate. (e.g. SearchRepo): " sceneName
+echo $sceneName
+
+read -p "module name to generate new feature scene for. (e.g. SearchRepoFeatures): " moduleName
+echo $moduleName
+
+# モジュールの存在確認
+destination="${PWD}/App/$moduleName/Sources/Scene"
+echo "destination: $destination"
+
+if [ -d "$destination" ]; then
+    swift run -c release --package-path ./Tools/GenesisTool genesis generate Genesis/Feature/template.yml\
+    -d "$destination"\
+    -o "sceneName: $sceneName, moduleName: $moduleName, date: $today, year: $current_year, developerName: $developer_name, developerCompany: $developer_company"
+
+    echo "$moduleName モジュールにsceneソースコードを出力しました $destination/$sceneName"
+else
+    swift run -c release --package-path ./Tools/GenesisTool genesis generate Genesis/Feature/template.yml\
+    -d Genesis/Feature/output/$moduleName\
+    -o "sceneName: $sceneName, moduleName: $moduleName, date: $today, year: $current_year, developerName: $developer_name, developerCompany: $developer_company"
+
+    echo "モジュールが存在しないので、Genesis/Feature/output/$moduleName/$sceneName に出力しました"
+fi
